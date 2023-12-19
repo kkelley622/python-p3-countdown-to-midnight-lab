25191a91c669ca2a6d7a526452946693594faf68
Kevin Kelley <kkelley622@gmail.com>
2023-12-18 21:37:04 -0600

    All tests are passing.

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,15 @@
+from time import sleep
+
 # your code goes here!
+def countdown(number):
+    while number >= 1:
+        print(f'{number} SECOND(S)!')
+        number -= 1
+    else:
+        print('HAPPY NEW YEAR!')
+def countdown_with_sleep(number):
+    while number:
+        print(f'{number} SECOND(S)!')
+        number -= 1
+        sleep(1)
+    print ('HAPPY NEW YEAR!')
